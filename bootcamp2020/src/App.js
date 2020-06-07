236cfe6201d32ee84b7ccfc5f4466292d74cfbd9
Hussnain Shahzad <66270127+hussnain202@users.noreply.github.com>
2020-06-07 17:57:52 +0500

    Class2Reactapp

---
+++ bootcamp2020/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      Hello Hussnain Shahzad!
+      
+    </div>
+  );
+}
+
+export default App;
